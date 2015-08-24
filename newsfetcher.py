2f26a1e81e361c4c143fe234ab471d273566ce9d
ilevn <nilsntth@gmail.com>
2015-08-24 13:07:51 +0200

    Testing general API functionality

--- newsfetcher.py
+++ newsfetcher.py
@@ -1,2 +1,18 @@
 __author__ = 'Nils'
 
+import praw
+
+def main():
+
+    r = praw.Reddit(user_agent='Newsfetcher by /u/vicerunt')
+    submissions = r.get_subreddit('tf2').get_top(limit=30)
+    submission_form = "{}) {} : {} <{}>"
+    count = 1
+    print("Top 30 Posts from /r/tf2")
+    print('-' * 25)
+    for sub in submissions:
+        print(submission_form.format(count, sub.ups, sub.title, sub.url))
+        count +=1
+
+if __name__ == "__main__":
+    main()
