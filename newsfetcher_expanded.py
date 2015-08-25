9688b9677068a93717abd0c13fa82e84281a1c69
ilevn <nilsntth@gmail.com>
2015-08-25 16:23:56 +0200

    Added the freedom to freely choose your desired sub reddit

--- newsfetcher_expanded.py
+++ newsfetcher_expanded.py
@@ -9,8 +9,11 @@ def main():
 
     r = praw.Reddit(user_agent='Newsfetcher by /u/vicerunt')
     print("Choose your desired subreddit:")
+    # Stores the entered subreddit in userchoice.
     userchoice = input("Your choice: ")
+    # Gets 30 top posts from the specific subreddit.
     submissions = r.get_subreddit(userchoice).get_top(limit=30)
+    # Formats the printed output and adds a nice counter:
     submission_form = "{}) {} : {} <{}>"
     count = 1
     print("Top 30 Posts from", userchoice)
@@ -21,4 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
